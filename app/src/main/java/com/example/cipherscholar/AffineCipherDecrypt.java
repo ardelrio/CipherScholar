a9f997ccd58017d92278a96d6e7f31d204247963
Hannah Beers <hannahlee0902@gmail.com>
2020-04-04 13:34:27 -0500

    Affine encrypt and decrypt functionality and Vigenere error checking

--- app/src/main/java/com/example/cipherscholar/AffineCipherDecrypt.java
+++ app/src/main/java/com/example/cipherscholar/AffineCipherDecrypt.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AffineCipherDecrypt extends AppCompatActivity {
 
@@ -20,6 +23,50 @@ public class AffineCipherDecrypt extends AppCompatActivity {
     }
 
     public void clickDecryptFunction(View view) {
-        //TODO add decrypt functionality
+        EditText plaintext = (EditText) findViewById(R.id.editText);
+        EditText key1 = (EditText) findViewById(R.id.editText2);
+        EditText key2 = (EditText) findViewById(R.id.editText3);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        String p = plaintext.getText().toString();  // p = plaintext as string
+        int k1 = Integer.parseInt(key1.getText().toString());
+        int k2 = Integer.parseInt(key2.getText().toString());
+
+        // make sure everything is in the correct form
+        if (p.length() < 1) {
+            Toast.makeText(this, "Incorrect formatting of input", Toast.LENGTH_SHORT).show();
+        }
+        else if (k1 < 0 || k1 > 26) {
+            Toast.makeText(this, "Enter a number 0-26", Toast.LENGTH_SHORT).show();
+        }
+        else if (k2 < 0 || k2 > 26) {
+            Toast.makeText(this, "Enter a number 0-26", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            /* convert plaintext to numbers  */
+            PlaintextCleaner pc = new PlaintextCleaner();
+            p = pc.removeExcess(p);
+            int[] pt = pc.convertToNum(p);  // pt = plaintext as number
+
+            int[] encrypted_int = new int[p.length()];  // final encrypted word as int
+            for (int i = 0; i < p.length(); i++) {
+                encrypted_int[i] = (modInverse(k1) * (pt[i] - k2)) % 26;
+            }
+
+            /* Convert cipher-text to string */
+            String encrypted_string = pc.convertToString(encrypted_int);  // final encrypted word as string
+            plaintext.setText(encrypted_string);
+            textView.setText("Plaintext -> Ciphertext");
+        }
+    }
+
+    static int modInverse(int a)
+    {
+        a = a % 26;
+        for (int x = 1; x < 26; x++)
+            if ((a * x) % 26 == 1)
+                return x;
+        return 1;
     }
 }
--- app/src/main/java/com/example/cipherscholar/VigenereCipher.java
+++ app/src/main/java/com/example/cipherscholar/VigenereCipher.java
@@ -36,24 +36,26 @@ public class VigenereCipher extends AppCompatActivity {
         if (k_string.length() < 1 || p.length() < 1) {
             Toast.makeText(this, "Incorrect formatting of input", Toast.LENGTH_SHORT).show();
         }
+        else {
 
-        /* convert plaintext to numbers  */
-        PlaintextCleaner pc = new PlaintextCleaner();
-        p = pc.removeExcess(p);
-        int[] pt = pc.convertToNum(p);  // pt = plaintext as number
+            /* convert plaintext to numbers  */
+            PlaintextCleaner pc = new PlaintextCleaner();
+            p = pc.removeExcess(p);
+            int[] pt = pc.convertToNum(p);  // pt = plaintext as number
 
-        /* convert key to numbers  */
-        k_string = pc.removeExcess(k_string);
-        int[] k = pc.convertToNum(k_string);  // k = key as number
+            /* convert key to numbers  */
+            k_string = pc.removeExcess(k_string);
+            int[] k = pc.convertToNum(k_string);  // k = key as number
 
-        int[] encrypted_int = new int[p.length()];  // final encrypted word as int
-        for(int i = 0; i < p.length(); i++) {
-            encrypted_int[i] = (pt[i] + k[i % k.length]) % 26;
+            int[] encrypted_int = new int[p.length()];  // final encrypted word as int
+            for (int i = 0; i < p.length(); i++) {
+                encrypted_int[i] = (pt[i] + k[i % k.length]) % 26;
+            }
+
+            /* Convert cipher-text to string */
+            String encrypted_string = pc.convertToString(encrypted_int);  // final encrypted word as string
+            plaintext.setText(encrypted_string);
+            textView.setText("Plaintext -> Ciphertext");
         }
-
-        /* Convert cipher-text to string */
-        String encrypted_string = pc.convertToString(encrypted_int);  // final encrypted word as string
-        plaintext.setText(encrypted_string);
-        textView.setText("Plaintext -> Ciphertext");
     }
 }
