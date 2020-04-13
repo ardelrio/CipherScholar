cf35fbdf5644d5ec1bc8f8bda86214f56363564e
Peter <pswanson2@wisc.edu>
2020-04-13 16:15:58 -0500

    RSA is now ADFGX. Just finished the xml.

--- app/src/main/java/com/example/cipherscholar/RsaCipher.java
+++ app/src/main/java/com/example/cipherscholar/RsaCipher.java
@@ -27,69 +27,9 @@ public class RsaCipher extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /**
-     * Generate random large prime numbers p and q
-     * @param view idk what the point of this is but ok
-     */
-    public void generate(View view) {
-        EditText e1 = (EditText) findViewById(R.id.editText2);
-        EditText e2 = (EditText) findViewById(R.id.editText3);
-
-        p = BigInteger.probablePrime(160, new Random());
-        q = BigInteger.probablePrime(160, new Random());
-
-        e1.setText(p.toString());
-        e2.setText(q.toString());
-    }
 
     public void clickEncryptFunction(View view) {
-        if (p == null || q == null) { return; }  // TODO maybe only accept generated values of p and q?
 
-        BigInteger n = p.multiply(q);
-        BigInteger p_m = p.subtract(BigInteger.ONE);
-        BigInteger q_m = q.subtract(BigInteger.ONE);
-        BigInteger phi = p_m.multiply(q_m);
-
-        /* Find e s.t. e is coprime to phi */
-        BigInteger it = BigInteger.ZERO;
-        BigInteger e = BigInteger.ZERO;
-        while (!it.equals(BigInteger.ONE)) {
-            e = new BigInteger(phi.bitLength(), new Random());
-            it = gcd(e, phi);
-        }
-
-        /* find e s.t. ed = 1 mod phi */
-        BigInteger d = e.modInverse(phi);
-
-        /* ciphertext = message^e mod n */
-        int e_int = e.intValue();
-        int n_int = n.intValue();
-
-        EditText plaintext = (EditText) findViewById(R.id.editText);
-        String pt = plaintext.getText().toString();
-
-        PlaintextCleaner pc = new PlaintextCleaner();
-
-        pt = pc.removeExcess(pt);
-        int[] pt_num = pc.convertToNum(pt);
-        int[] ct = new int[pt_num.length];
-        for (int i = 0; i < pt_num.length; i++) {
-            ct[i] = ((int) Math.pow(pt_num[i], e_int)) % n_int;
-        }
-
-        String ct_str = pc.convertToString(ct);
-        plaintext.setText(ct_str);
-    }
-
-    /**
-     * Find the greatest common divisor of two BigIntegers
-     * @param a
-     * @param b
-     * @return the gcd
-     */
-    public BigInteger gcd(BigInteger a, BigInteger b) {
-        if (b.equals(BigInteger.ZERO)) return a;
-        else return gcd(b, a.mod(b));
     }
 
 }
